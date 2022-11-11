f3daa737228c95f478b0e729028368954f7249e9
Vybornyi <1vybornyi@gmail.com>
2022-11-11 12:54:05 +0200

    add

--- lesson-6/task-1/src/store.js
+++ lesson-6/task-1/src/store.js
@@ -3,7 +3,7 @@ import counterReducer from './counter.reducer';
 
 const logger = store => next => action => {
   console.group(action.type);
-  console.info(action);
+  console.info('dispatching', action.type);
   const result = next(action);
   console.log('next state', store.getState());
   console.groupEnd();
